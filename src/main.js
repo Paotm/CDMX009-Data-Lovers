298a17d50979324272d45de11b8f8d7d3d79a082
Paolatm <ipao.tm2202@gmail.com>
2020-07-15 21:58:56 -0500

    update without styles

--- src/main.js
+++ src/main.js
@@ -1,4 +1,30 @@
-import { example } from './data.js';
+import {getCharacterbyType} from './data.js';
+import data from './data/pokemon/pokemon.js';
 
-console.log(example);
+// let list= data.pokemon;
+//Nodo
+const types = document.querySelectorAll(".type");
+const poke = document.querySelector('#pokes');
 
+//Listener
+types.forEach(b=>b.addEventListener("click", e=>{createCard(getCharacterbyType(e.target.id))
+}
+))
+
+
+ function createCard(array) {
+   poke.innerHTML = '';
+  for (let pokemon of array) {
+
+  poke.innerHTML += `
+<div id="uno" class="card">
+  <img class="imag" src=${pokemon.img} />
+    <p>${pokemon.name}</p>
+
+  <p>Numero: ${pokemon.num}</p>
+  <p> Tipo de Dulce: ${pokemon.candy}</p>
+  <p>Dulces para evolucion: ${pokemon.candy_count}</p>
+  </div>
+  `
+ }
+};
